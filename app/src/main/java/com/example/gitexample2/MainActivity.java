0f2967919b01713eb9c769ce8ef716b56609a6e6
Seba <pezkaos@gmail.com>
2020-08-11 11:33:48 -0500

    ADD: Primer Commit test colaborativo github

---
+++ app/src/main/java/com/example/gitexample2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.gitexample2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String name = "Hoy es Lunes";
+
+    }
+}
